f1c5d7af3e574eac023f33043c942bc9678a41ef
Ankit Nayan <ankit@signoz.io>
2022-04-25 17:19:48 +0530

    Chore/improve ch perf (#22)
    * chore: added metric_name when writing samples

--- exporter/clickhousemetricsexporter/exporter.go
+++ exporter/clickhousemetricsexporter/exporter.go
@@ -26,7 +26,6 @@ import (
 
 	"github.com/pkg/errors"
 	"go.uber.org/multierr"
-	"go.uber.org/zap"
 
 	"github.com/prometheus/prometheus/prompb"
 
@@ -71,14 +70,13 @@ func NewPrwExporter(cfg *Config, set component.ExporterCreateSettings) (*PrwExpo
 	userAgentHeader := fmt.Sprintf("%s/%s", strings.ReplaceAll(strings.ToLower(set.BuildInfo.Description), " ", "-"), set.BuildInfo.Version)
 
 	params := &ClickHouseParams{
-		DSN:                  cfg.HTTPClientSettings.Endpoint,
-		DropDatabase:         false,
-		MaxOpenConns:         75,
-		MaxTimeSeriesInQuery: 50,
+		DSN:          cfg.HTTPClientSettings.Endpoint,
+		DropDatabase: false,
+		MaxOpenConns: 75,
 	}
 	ch, err := NewClickHouse(params)
 	if err != nil {
-		zap.S().Error("couldn't create instance of clickhouse")
+		return nil, err
 	}
 
 	return &PrwExporter{
