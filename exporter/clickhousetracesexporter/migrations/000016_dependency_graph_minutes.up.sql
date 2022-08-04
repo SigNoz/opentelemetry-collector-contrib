ce731936f7270592a7092eac9a173b58f180e867
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-08-04 09:06:59 +0530

    chore: rename column

--- exporter/clickhousetracesexporter/migrations/000016_dependency_graph_minutes.up.sql
+++ exporter/clickhousetracesexporter/migrations/000016_dependency_graph_minutes.up.sql
@@ -4,10 +4,10 @@ CREATE TABLE IF NOT EXISTS signoz_traces.dependency_graph_minutes (
     duration_quantiles_state AggregateFunction(quantiles(0.5, 0.75, 0.9, 0.95, 0.99), Float64) CODEC(Default),
     error_count SimpleAggregateFunction(sum, UInt64) CODEC(T64, ZSTD(1)),
     total_count SimpleAggregateFunction(sum, UInt64) CODEC(T64, ZSTD(1)),
-    time DateTime CODEC(Delta(4), Default)
+    timestamp DateTime CODEC(Delta(4), Default)
 ) ENGINE AggregatingMergeTree()
-PARTITION BY toDate(time)
-ORDER BY (time, src, dest);
+PARTITION BY toDate(timestamp)
+ORDER BY (timestamp, src, dest);
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS signoz_traces.dependency_graph_minutes_service_calls_mv
 TO signoz_traces.dependency_graph_minutes AS
@@ -17,10 +17,10 @@ SELECT
     quantilesState(0.5, 0.75, 0.9, 0.95, 0.99)(toFloat64(B.durationNano)) as duration_quantiles_state,
     countIf(B.statusCode=2) as error_count,
     count(*) as total_count,
-    toStartOfMinute(B.timestamp) as time
+    toStartOfMinute(B.timestamp) as timestamp
 FROM signoz_traces.signoz_index_v2 AS A, signoz_traces.signoz_index_v2 AS B
 WHERE (A.serviceName != B.serviceName) AND (A.spanID = B.parentSpanID)
-GROUP BY (time, src, dest);
+GROUP BY (timestamp, src, dest);
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS signoz_traces.dependency_graph_minutes_db_calls_mv
 TO signoz_traces.dependency_graph_minutes AS
@@ -30,10 +30,10 @@ SELECT
     quantilesState(0.5, 0.75, 0.9, 0.95, 0.99)(toFloat64(durationNano)) as duration_quantiles_state,
     countIf(statusCode=2) as error_count,
     count(*) as total_count,
-    toStartOfMinute(timestamp) as time
+    toStartOfMinute(timestamp) as timestamp
 FROM signoz_traces.signoz_index_v2
 WHERE dest != '' and kind != 2
-GROUP BY (time, src, dest);
+GROUP BY (timestamp, src, dest);
 
 CREATE MATERIALIZED VIEW IF NOT EXISTS signoz_traces.dependency_graph_minutes_messaging_calls_mv
 TO signoz_traces.dependency_graph_minutes AS
@@ -43,7 +43,7 @@ SELECT
     quantilesState(0.5, 0.75, 0.9, 0.95, 0.99)(toFloat64(durationNano)) as duration_quantiles_state,
     countIf(statusCode=2) as error_count,
     count(*) as total_count,
-    toStartOfMinute(timestamp) as time
+    toStartOfMinute(timestamp) as timestamp
 FROM signoz_traces.signoz_index_v2
 WHERE dest != '' and kind != 2
-GROUP BY (time, src, dest);
+GROUP BY (timestamp, src, dest);
