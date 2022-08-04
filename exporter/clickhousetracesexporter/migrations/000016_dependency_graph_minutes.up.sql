586d14f1147c856848a3c887418cac6f7ccc80cb
Srikanth Chekuri <srikanth.chekuri92@gmail.com>
2022-08-04 13:01:32 +0530

    chore: update compression type

--- exporter/clickhousetracesexporter/migrations/000016_dependency_graph_minutes.up.sql
+++ exporter/clickhousetracesexporter/migrations/000016_dependency_graph_minutes.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS signoz_traces.dependency_graph_minutes (
     duration_quantiles_state AggregateFunction(quantiles(0.5, 0.75, 0.9, 0.95, 0.99), Float64) CODEC(Default),
     error_count SimpleAggregateFunction(sum, UInt64) CODEC(T64, ZSTD(1)),
     total_count SimpleAggregateFunction(sum, UInt64) CODEC(T64, ZSTD(1)),
-    timestamp DateTime CODEC(Delta(4), Default)
+    timestamp DateTime CODEC(DoubleDelta, LZ4)
 ) ENGINE AggregatingMergeTree()
 PARTITION BY toDate(timestamp)
 ORDER BY (timestamp, src, dest);
